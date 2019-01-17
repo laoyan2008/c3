1e628d703fbce0a7efff3c0a7ff127a80dfcbb00
yy <1019624863@qq.com>
2019-01-17 17:04:06 +0800

    添加增加车辆位置功能

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
+
 struct Point
 {
     int x;
@@ -7,13 +8,13 @@ struct Point
 };
 int main()
 {
-    int index = 0;
+    int index=0;
     struct Point arr[100];
-    printf("欢迎进入车辆位置管理系统!\n");
+    printf("欢迎进入车辆位置管理系统\n");
 
-    while (1)
+    while(1)
     {
-        printf("1---添加车辆位置\n");
+         printf("1---添加车辆位置\n");
         printf("2---删除车辆位置\n");
         printf("3---显示全部车辆位置\n");
         printf("4---返回离我位置最近的车辆\n");
@@ -22,8 +23,7 @@ int main()
         printf("请选择相应功能\n");
         int code;
         scanf("%d", &code);
-
-        if (code == 1)
+        if(code==1)
         {
             printf("请输入车辆位置的x坐标与y坐标：\n");
             int x;
@@ -41,67 +41,21 @@ int main()
             scanf("%c", &xx);
             scanf("%c", &xx);
         }
-        if (code == 2)
+        if(code==2)
         {
-            if (index > 0)
-            {
-                index--;
-                printf("删除完成，点击回车继续\n");
-                char xx;
-                scanf("%c", &xx);
-                scanf("%c", &xx);
-            }
-            else
-            {
-                printf("无车辆位置信息，删除失败，点击回车继续\n");
-                char xx;
-                scanf("%c", &xx);
-                scanf("%c", &xx);
-            }
+           
         }
-        if (code == 3)
+        if(code==3)
         {
-            printf("所有车辆位置如下：\n");
-
-            for (int i = 0; i < index; i++)
-            {
-                printf("第%d个车辆的位置为(%d,%d)\n", i + 1, arr[i].x, arr[i].y);
-            }
-            printf("\n点击回车继续\n");
-            char xx;
-            scanf("%c", &xx);
-            scanf("%c", &xx);
+            
         }
-        if (code == 4)
+        if(code==4)
         {
-            printf("请输入当前位置坐标：\n");
-            int x;
-            int y;
-            scanf("%d",&x);
-            scanf("%d",&y);
-            double min = 100000;
-            int carID = 0;
-            for(int i=0;i<index;i++)
-            {
-                int dx = x - arr[i].x;
-                int dy = y - arr[i].y;
-                double l = sqrt(dx*dx+dy*dy);
-                if(min > l)
-                {
-                    min = l;
-                    carID = i;
-                }
-            }
-            printf("用户当前位置与第%d个车的距离为%lf，是最小距离\n",carID+1,min);
-            printf("\n点击回车继续\n");
-            char xx;
-            scanf("%c", &xx);
-            scanf("%c", &xx);
+            
         }
-        if (code == 5)
+        if(code==5)
         {
-            printf("应用已经退出\n");
-            break;
+            
         }
     }
     return 0;
